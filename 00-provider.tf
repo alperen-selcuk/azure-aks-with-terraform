dcdec128a18836b1ef878c61af0ef45b30e4276d
Alperen Selçuk <alperen.selcuk@setur.com.tr>
2023-11-20 11:08:31 +0300

    first

---
+++ 00-provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.81.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
