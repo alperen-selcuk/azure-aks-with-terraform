8d23020f541f573a5a665f91e12b8493d0234516
Alperen Selçuk <alperen.selcuk@setur.com.tr>
2023-11-21 00:22:17 +0300

    file update

--- 00-provider.tf
+++ 00-provider.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.81.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {
+  }
 }
