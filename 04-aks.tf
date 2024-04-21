593a6ddd584284195139255defa5a63732c2e394
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2024-04-21 22:19:04 +0300

    Update 04-aks.tf

--- 04-aks.tf
+++ 04-aks.tf
@@ -30,13 +30,3 @@ resource "azurerm_kubernetes_cluster" "production" {
 
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.production.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.production.kube_config_raw
-
-  sensitive = true
-}
